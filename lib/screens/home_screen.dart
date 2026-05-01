f97e834b7e4b133455e76f6983c0e8d08da71cd4
Salem Alawlaki <salemalawlaki9@gmail.com>
2026-05-01 16:05:49 +0300

    Stage 02 - add new updates

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -71,6 +71,132 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
+          _buildMovieCard(
+            title: 'Inception',
+            subtitle: 'Sci-Fi • 2010',
+            rating: '8.5',
+          ),
+          // استدعاء الدالة لإنشاء بطاقة الفيلم الثانية
+          _buildMovieCard(
+            title: 'The Matrix',
+            subtitle: 'Sci-Fi • 1999',
+            rating: '8.7',
+          ),
+          // استدعاء الدالة لإنشاء بطاقة الفيلم الثالثة
+          _buildMovieCard(
+            title: 'Interstellar',
+            subtitle: 'Sci-Fi • 2014',
+            rating: '8.6',
+          ),
+          // استدعاء الدالة لإنشاء بطاقة الفيلم الربع
+          _buildMovieCard(
+            title: 'The Tomorrow War',
+            subtitle: 'Sci-Fi •2021',
+            rating: '7.5',
+          ),
+        ],
+      ),
+    );
+  }
+
+  // دالة ترجع بطاقة فيلم
+  Widget _buildMovieCard({
+    //دالة من نوع ويدجت
+    //عند استدعاء الدالة لازم تعطيها هذه القيم.
+    /*
+   في هذه المرحلة حولت بطاقة الفيلم إلى دالة نوعها Widget
+    حتى أقلل تكرار الكود. جعلت الدالة تستقبل 
+   title و subtitle و rating باستخدام required، 
+   ثم استخدمت هذه القيم داخل Text. وأضفت Spacer قبل صندوق التقييم حتى يظهر التقييم في يمين البطاقة.
+   */
+    required String title,
+    required String subtitle,
+    required String rating,
+  }) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      padding: const EdgeInsets.all(16),
+
+      decoration: BoxDecoration(
+        color: Colors.white,
+
+        // جعل زوايا البطاقة دائرية
+        borderRadius: BorderRadius.circular(12),
+
+        // إضافة ظل للبطاقة
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade300,
+            blurRadius: 4,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+        children: [
+          // صندوق الأيقونة
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple.shade100,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const Icon(Icons.movie, color: Colors.deepPurple),
+          ),
+
+          // مسافة بين الأيقونة والنصوص
+          const SizedBox(width: 16),
+
+          // اسم الفيلم والنوع
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              Text(
+                subtitle,
+                style: TextStyle(fontSize: 14, color: Colors.grey.shade600),
+              ),
+            ],
+          ),
+
+          // يدفع التقييم إلى أقصى اليمين
+          const Spacer(),
+
+          // صندوق التقييم
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade400,
+                  blurRadius: 4,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Text(
+              rating,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 13,
+              ),
+            ),
+          ),
         ],
       ),
     );
